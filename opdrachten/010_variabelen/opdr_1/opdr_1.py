7b662cad2874d82395c8051d6482d2215044f363
Jacob9708 <97087437@st.deltion.nl>
2024-09-20 12:30:57 +0200

    Done

--- opdrachten/010_variabelen/opdr_1/opdr_1.py
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -1,7 +1,8 @@
 # Opdracht 1
-# Naam student:
-# Groep:
+# Naam student:Jacob van de Berg
+# Groep:4ITX
 
 # Hier komt je code...
 
-my_list = ...
+my_list = my_list = list(["13", "wat een weertje", "12.5", "8"])
+print(my_list)

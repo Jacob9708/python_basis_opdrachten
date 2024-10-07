e50bd3378706a2f258ad1ce42444113754d08263
jacob9708 <97087437@st.deltion.nl>
2024-10-07 08:24:25 +0200

    Opdracht 3

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -2,5 +2,12 @@
 # Naam student:Jacob
 # Groep:4ITX
 
-# Hier komt je code...
+leeftijd_joe = int("23")
+leeftijd_bob = int("15")
+leeftijd_fred = int("22")
 
+totaal_leeftijd = leeftijd_joe + leeftijd_bob + leeftijd_fred
+gemiddelde_leeftijd = totaal_leeftijd /3
+
+print(f"Leeftijden totaal: {totaal_leeftijd}")
+print(f"Gemiddelde leeftijd: {gemiddelde_leeftijd:.2f}")

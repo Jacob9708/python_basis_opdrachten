5828e5620fd0381a1cafb2450171c084cc72e2fa
jacob9708 <97087437@st.deltion.nl>
2024-10-10 14:05:20 +0200

    Jacob

--- opdrachten/010_variabelen/opdr_1/opdr_1.py
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -4,5 +4,5 @@
 
 # Hier komt je code...
 
-my_list = my_list = my_list = list(["13", "wat een weertje", "12.5", "8"])
+my_list = list(["13", "wat een weertje", "12.5", "8"])
 print(my_list)
--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -1,10 +1,34 @@
 # Opdracht 2 berekeningen
-# Naam student:
+# Naam student:Jacob van de Berg
 # Groep:
 
 # Hier komt je code...
 
-c = ...
-f = ...
+c = -12
+f = 102
+
+# Celsius naar fahrenheit
+fahrenheit = (c * 9/5) + 32
+
+# Fahrenheit naar Celsius
+celcius = (f - 32) * 5/9
+
+# Toon de resultaten
+print(f"{c}°C is gelijk aan {fahrenheit:.2f}°F")
+print(f"{f}°F is gelijk aan {celcius:.2f}°C")
+
+c = 62.2
+f = 92
+
+# Celsius naar fahrenheit
+fahrenheit = (c * 9/5) + 32
+
+# Fahrenheit naar Celsius
+celcius = (f - 32) * 5/9
+
+# Toon de resultaten
+print(f"{c}°C is gelijk aan {fahrenheit:.2f}°F")
+print(f"{f}°F is gelijk aan {celcius:.2f}°C")
+
 
 print()
